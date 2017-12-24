fec43eb8591d5ed88ce023f8cb634b33cd8862ab
JackieW001 <jwoo@stuy.edu>
2017-12-24 14:53:31 -0500

    added cookie button and click value upgrade

--- utils/cc.js
+++ utils/cc.js
@@ -1,9 +1,40 @@
 var cookieBank = 0;
-//Cookie
-var addToCookieBank = function(cookies){
-    cookieBank += cookies;
-};
+var cookieBankHTML = document.getElementById("cookieBank");
 
 var cookie = document.getElementById("cookie");
-cookie.addEventListener("onclick", function(e){ addToCookieBank(10); });
+
+var clickVal = 1;
+var upClickValHTML = document.getElementById("upClickVal");
+var upClickVal = 2; // multiplies current click val with this val when upgraded
+var upClickValCost = 50;
+
+var printsmth = function(e){
+    console.log("clicked button");
+};
+var updateCB = function (cookieBankValue){
+    cookieBankHTML.innerHTML = "Cookies: " + cookieBank;
+}
+
+var addToCookieBank = function(cookies){
+    cookieBank += cookies;
+    console.log("Cookie bank: " + cookieBank);
+    updateCB(cookieBank);
+};
+
+cookie.addEventListener('click', function() {addToCookieBank(clickVal);} );
+
+var upgradeClickVal = function(val){
+    if (cookieBank - upClickValCost > 0){
+	// take out cost of upgrade
+	cookieBank -= upClickValCost;
+	updateCB(cookieBank);
+	// increase click val
+	clickVal *= upClickVal;
+	// increase upgrade cost
+	upClickValCost *= 2;
+    }
+};
+
+upClickValHTML.addEventListener('click', function() { upgradeClickVal(upClickVal);});
+
 
