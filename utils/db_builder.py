c7c95f98a5e6f31ec2bc9beee0c47f3ea720be14
Manahal Tabassum <mtabassum@stuy.edu>
2017-12-30 12:27:39 -0500

    worked on db_builder

--- utils/db_builder.py
+++ utils/db_builder.py
@@ -3,20 +3,23 @@ import csv
 import hashlib
 import uuid
 
+#still need to implement generators stuff
 
 #==========================================================
 '''
 TABLE CREATION
 Database info.db
-Tables: users, stories, updates
+Tables: credentials
 '''
 
 def tableCreation():
     f = "data/info.db"
     db = sqlite3.connect(f)
     c = db.cursor()
-    user_table = 'CREATE TABLE users (username TEXT, password BLOB);'
+    user_table = 'CREATE TABLE credentials (id INTEGER, username TEXT, password BLOB);'
     c.execute(user_table)
+    stats_table = 'CREATE TABLE stats (id INTEGER, cookies INTEGER, cps INTEGER, generators BLOB);'
+    c.execute(stats_table)
     db.commit()
     db.close()
 
@@ -33,8 +36,13 @@ def addUser(new_username, new_password):
     f="data/info.db"
     db = sqlite3.connect(f) #open if f exists, otherwise create
     c = db.cursor()         #facilitates db ops
+    userCount = c.execute('SELECT COUNT(*) FROM credentials;')
+    new_userID = 0
+    for x in userCount:
+        new_userID = x[0]
     hash_pass = hash_password(new_password)
-    c.execute('INSERT INTO users VALUES (?,?)',[new_username, hash_pass])
+    c.execute('INSERT INTO credentials VALUES (?,?,?)',[new_userID, new_username, hash_pass])
+    c.execute('INSERT INTO stats VALUES (?,?,?,?)',[new_userID, 0, 0, 0])
     db.commit()
     db.close()
 
@@ -61,11 +69,12 @@ def authenticate(user, passw):
 
 #ACCESSORS
 
+#accessor to get password based on username
 def getPass(username):
     f="data/info.db"
     db = sqlite3.connect(f) #open if f exists, otherwise create
     c = db.cursor()         #facilitates db ops
-    command = "SELECT username, password FROM users"
+    command = "SELECT username, password FROM credentials;"
     info = c.execute(command)
     retVal = None
     for entry in info:
@@ -74,7 +83,68 @@ def getPass(username):
     db.close()
     return retVal
 
+
+def getUserID(username):
+    f = "data/info.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    command = "SELECT username, id FROM credentials"
+    info = c.execute(command)
+    retVal = None
+    for user in info:
+        if str(user[0]) == username:
+            retVal = str(user[1])
+    db.close()
+    return retVal
+
+def getCookies(username):
+    f = "data/info.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    user_id = getUserID(username)
+    cookies = c.execute('SELECT cookies FROM stats WHERE id= "' + user_id + '";')
+    retVal = (cookies.fetchone())[0]
+    db.close()
+    return retVal
+
+def getCPS(username):
+    f="data/info.db"
+    db = sqlite3.connect(f) #open if f exists, otherwise create
+    c = db.cursor()         #facilitates db ops
+    user_id = getUserID(username)
+    cps_val = c.execute('SELECT cps FROM stats WHERE id = ' + str(user_id) + ';')
+    retVal = (cps_val.fetchone())[0]
+    db.close()
+    return retVal
+
+
 #================================================================
+
+#MUTATORS
+
+def addCookies(user, c_count):
+    f="data/info.db"
+    db = sqlite3.connect(f) #open if f exists, otherwise create
+    c = db.cursor()         #facilitates db ops
+    user_id = getUserID(user)
+    stuff = c.execute('SELECT cookies FROM stats WHERE id= ' + user_id + ';')
+    old_content = stuff.fetchone()
+    #print 'OLD_CONTENT...'
+    #print old_content
+    cookies = old_content[0] + c_count
+    c.execute("UPDATE stats SET cookies =" + str(cookies) + " WHERE id = " + user_id)
+    db.commit()
+    db.close()
+
+def setCPS(user, new_cps):
+    f="data/info.db"
+    db = sqlite3.connect(f) #open if f exists, otherwise create
+    c = db.cursor()         #facilitates db ops
+    user_id = getUserID(user)
+    cps = str(new_cps)
+    c.execute("UPDATE stats SET cps = " + cps + " WHERE id = " + user_id)
+    db.commit()
+    db.close()
 
 #TESTING
 
@@ -86,11 +156,28 @@ if __name__ == '__main__':
     addUser('manahal','tabassum')
     addUser('bob', '123')
 
+    print getUserID('manahal')
+    print getUserID('bob')
+
     #check getPass
+    print 'manahal pass:'
     print getPass('manahal')
-    print getPass('joe')
+    print 'bob pass:'
+    print getPass('bob')
 
     #check authenticate
     authenticate('manahal','tabassum')
     authenticate('joe','123')
     authenticate('bob','sdilblf')
+
+    print 'original cookie count manahal'
+    print getCookies('manahal')
+    addCookies('manahal', 100)
+    print 'new cookie count manahal'
+    print getCookies('manahal')
+
+    print 'original cps manahal'
+    print getCPS('manahal')
+    setCPS('manahal', 50)
+    print 'new cps manahal'
+    print getCPS('manahal')
