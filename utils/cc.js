c1292bc9e2910c0762dec5ad12f7a5c3d9f31037
JackieW001 <jwoo@stuy.edu>
2018-01-06 15:07:19 -0500

    scaled gen costs

--- utils/cc.js
+++ utils/cc.js
@@ -49,11 +49,13 @@ var generatorfuntimes = function(gen, html){
     if (cookieBank-gen.cost >= 0){
 	addToCookieBank(-gen.cost);
 	gen.num += 1;
+	gen.updateCost();
 	setInterval(function(){
 	    addToCookieBank(gen.cps);
 	}, 1000);
-	html.innerHTML = "You have " + gen.num + " gen" + gen.id;
+	html.innerHTML = "Cost: " + Math.floor(gen.cost) + " --- Generates " + gen.cps + " cps. You have " + gen.num + " gen" + gen.id;
     }
+
 }
 
 class Generator {
@@ -63,15 +65,19 @@ class Generator {
 	this.cost = cost;
 	this.cps = cps;
     }
+    updateCost(){
+	this.cost *= 1.1;
+    }
+
 }
 var gen0button = document.getElementById("gen0");
-var gen0NumHTML = document.getElementById("gen0Num");
+var gen0HTML = document.getElementById("gen0p");
 var gen0 = new Generator(0,0, 100, 1);
 
-gen0button.addEventListener('click', function() { generatorfuntimes(gen0, gen0NumHTML);})
+gen0button.addEventListener('click', function() { generatorfuntimes(gen0, gen0HTML);})
 
 var gen1button = document.getElementById("gen1");
-var gen1NumHTML = document.getElementById("gen1Num");
+var gen1HTML = document.getElementById("gen1p");
 var gen1 = new Generator(1,0, 1100, 8);
 
 gen1button.addEventListener('click', function() { generatorfuntimes(gen1, gen1NumHTML);})
