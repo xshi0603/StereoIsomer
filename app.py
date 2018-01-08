0a51929a98e0bb71ccd69ad845716e86c29fa30a
Xing Tao Shi <xshi@stuy.edu>
2018-01-08 13:48:56 -0500

    cut comments

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 import os, csv, sqlite3, hashlib, uuid, requests
 from utils import  db_builder
-#from datetime import datetime
-#from utils import api_library, dbLibrary
-#import json
 
 #----------------------ENCRYPTION---------------------------
 
@@ -38,16 +35,10 @@ def login():
 
 @cookie_app.route("/authenticate",methods = ['POST','GET']) #old stuff
 def authenticate():
-    '''
-    dbTunes = dbLibrary.openDb("data/tunes.db")
-    cursor = dbLibrary.createCursor(dbTunes)
-    '''
-
     input_username = request.form['username']
     input_password = request.form['password']
 
     status = db_builder.authenticate(input_username, input_password)
-    
 
     if (status == 0):
         flash("user dne")
@@ -57,40 +48,6 @@ def authenticate():
     
     flash("password wrong")
     return redirect(url_for('login'))
-
-'''
-if input_username=='' or input_password=='' :
-        flash("Please Fill In All Fields")
-        return redirect(url_for('login'))
-
-    if "'" in input_username or "'" in input_password:
-        flash("Invalid Login Info")
-        return redirect(url_for('login'))
-
-    hashed_passCursor = cursor.execute("SELECT password FROM users WHERE username = '" + input_username + "'")
-    numPasses = 0 #should end up being 1 if all fields were filled
-
-    for item in hashed_passCursor:
-        numPasses += 1
-        hashed_pass = item[0]
-        print item[0]
-
-    dbLibrary.closeFile(dbTunes)
-
-    if  numPasses == 0:
-        flash ("User doesn't exist")
-        return redirect(url_for('login'))
-
-    elif check_password(hashed_pass, input_password):
-        flash("Login Successful")
-        session["username"] = input_username;#in order to keep track of user
-
-
-    else:
-        flash("Invalid Login Information")
-        return redirect(url_for('login'))
-'''
-    
     
 #-------------------------------------------------------------------
 
@@ -104,52 +61,10 @@ def account():
 
 @cookie_app.route("/accountSubmit", methods = ['POST' , 'GET'])
 def accountSubmit():
-    '''
-    dbTunes = dbLibrary.openDb("data/tunes.db")
-    cursor = dbLibrary.createCursor(dbTunes)
-    '''
-    #print request.form
     username = request.form['newUsername']
     password = request.form['newPassword']
-    print(username)
-    print(password)
+    
     db_builder.addUser(username, password)
-    '''
-    if username == '' or password == '':
-        dbLibrary.closeFile(dbTunes)
-        flash("Please Fill In All Fields")
-        return redirect(url_for('account'))
-
-    elif len(password)< 6:
-        dbLibrary.closeFile(dbTunes)
-        flash("Password must have at least 6 characters")
-        return redirect(url_for('account'))
-
-    elif (' ' in username or ' ' in password or "'" in username or "'" in password or '"' in username or '"' in password ):
-        dbLibrary.closeFile(dbTunes)
-        flash("Username and Password cannot contain the space,single quote, or double quote character")
-        return redirect(url_for('account'))
-
-    password = hash_password(password)
-    sameUser = cursor.execute("SELECT username FROM users WHERE username = '" + username +"'")
-
-    counter = 0 #should remain 0 if valid username since username needs to be unique
-    for item in sameUser:
-        counter += 1
-
-    if counter == 0:
-        dbLibrary.insertRow('users',['username', 'password', 'sadness', 'joy', 'anger', 'fear'],[username, password, "base", "base", "base", "base"],cursor)
-        flash("Account Successfully Created")
-        dbLibrary.commit(dbTunes)
-        dbLibrary.closeFile(dbTunes)
-        return redirect(url_for('login'))
-
-    else:
-        flash("Invalid: Username taken")
-        dbLibrary.commit(dbTunes)
-        dbLibrary.closeFile(dbTunes)
-
-    '''
     return redirect(url_for('login'))
     
 #-----------------------------------------------------------
@@ -181,16 +96,11 @@ def logout():
 #---------------------WEATHER----------------------------
 
 def getWeather():
-    key = open('wunderground_api_key.txt', 'rb').read()
-    #print key
-    #data = newJSON
-    #response = requests.get('http://api.wunderground.com/api/ab6209433554e030/conditions/q/NY/New_York_City.json')   
-    url = 'http://api.wunderground.com/api/' + key + '/conditions/q/CA/San_Francisco.json'                                                            
-    #print(url)
+    key = open('wunderground_api_key.txt', 'rb').read() 
+    url = 'http://api.wunderground.com/api/' + key + '/conditions/q/CA/San_Francisco.json'                         
     response = requests.get(url)
     response = response.json()
     temperature = response['current_observation']['temp_f']
-    #print temperature
     return temperature
 
 @cookie_app.route('/weather', methods=['GET'])
@@ -214,7 +124,6 @@ def leaderboard():
 def achievements():
     if 'username' in session:
         return render_template("achievements.html")
-    #not logged in
     flash("Please log in first")
     return redirect(url_for("root"))
 
